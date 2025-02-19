7fe09c96858f3433a88dab0f400e56516ae513c6
praj144 <154552716+praj144@users.noreply.github.com>
2025-02-19 10:49:36 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "bucket_name" {
+  description = "GCS bucket for Cloud Function code"
+  type        = string
+  default     = "my-function-bucket"
+}
